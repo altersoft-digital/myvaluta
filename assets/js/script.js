88b25a5f889d3b2b990eba44144753eefac06863
khimsh <i.khimsh@gmail.com>
2022-04-27 15:59:16 +0400

    currency picker js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,61 @@
   })
 
   // filter dropdown
-  function dropdown() {
+  function filterDropdown() {
     if (!document.querySelector('.filter-dropdown')) return
 
     const dropdown = document.querySelector('.filter-dropdown')
     const trigger = dropdown.querySelector('[dropdown-trigger]')
+    const options = dropdown.querySelectorAll('.filter-options button')
 
     trigger.addEventListener('click', () => {
       dropdown.classList.toggle('active')
     })
+
+    options.forEach((option) => {
+      option.addEventListener('click', () => {
+        // update filter button chosen option
+        const optionInnerContent = option.innerHTML
+        trigger.querySelector('div').innerHTML = optionInnerContent
+
+        // close dropdown
+        dropdown.classList.remove('active')
+
+        // submit api call from here
+        console.log(option.value)
+      })
+    })
   }
 
-  dropdown()
+  function currencyDropdown() {
+    if (!document.querySelector('.dropdown')) return
+
+    const dropdowns = document.querySelectorAll('.dropdown')
+
+    dropdowns.forEach((dropdown) => {
+      const trigger = dropdown.querySelector('[dropdown-trigger]')
+      const options = dropdown.querySelectorAll('.dropdown-options button')
+
+      trigger.addEventListener('click', () => {
+        dropdown.classList.toggle('active')
+      })
+
+      options.forEach((option) => {
+        option.addEventListener('click', () => {
+          // update filter button chosen option
+          const optionInnerContent = option.innerHTML
+          trigger.querySelector('div').innerHTML = optionInnerContent
+
+          // close dropdown
+          dropdown.classList.remove('active')
+
+          // submit api call from here
+          console.log(option.value)
+        })
+      })
+    })
+  }
+
+  currencyDropdown()
+  filterDropdown()
 })()

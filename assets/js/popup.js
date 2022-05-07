e60ed508f29c19105a329eb489f0908e748413ac
Giorgi Matsukatovi <giorgimatsukatov@gmail.com>
2022-05-07 13:02:59 +0400

    dwahduwad

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,11 @@
 function openpopup(popup) {
     document.querySelector('.bg-overlay').style.display = 'grid';
     document.querySelector(popup).style.display = 'grid';
+    document.querySelector('body').style.overflowY = 'hidden';
 }
 
 function closepopup(popup) {
     document.querySelector('.bg-overlay').style.display = 'none';
     document.querySelector(popup).style.display = 'none';
+    document.querySelector('body').style.overflowY = 'unset';
 }

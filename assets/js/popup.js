4bc01dc1231388d496337b55de678dc7b4bac89c
MariamTchelidze <mariam.tch1996@gmail.com>
2022-05-13 17:02:35 +0400

    added and edited tasks sent by owner

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -9,3 +9,19 @@ function closepopup(popup) {
     document.querySelector(popup).style.display = 'none';
     document.querySelector('body').style.overflowY = 'unset';
 }
+
+// checkbox hovers
+
+function myFunction(x, b, c, _this) {
+    if (_this.checked) {
+        document.querySelector(x).style.backgroundColor = '#5B9279';
+        document.querySelector(b).style.color = '#ffffff';
+        document.querySelector(c).style.color = '#ffffff';
+    } else  {
+        document.querySelector(x).style.backgroundColor = '#F4F5F4';
+        document.querySelector(b).style.color = '#263E33';
+        document.querySelector(c).style.color = '#65756E';
+    }
+  }
+
+

300088d523be778c10248236856a557fbd8e473a
Giorgi Matsukatovi <giorgimatsukatov@gmail.com>
2022-05-23 11:35:20 +0400

    dhwaduwa

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,6 +2,7 @@ function openpopup(popup) {
     document.querySelector('.bg-overlay').style.display = 'grid';
     document.querySelector(popup).style.display = 'grid';
     document.querySelector('body').style.overflowY = 'hidden';
+    document.querySelector('.bg-overlay').style.background = 'rgba(14, 12, 19, 0.85)';
 }
 
 function closepopup(popup) {
@@ -10,9 +11,9 @@ function closepopup(popup) {
     document.querySelector('body').style.overflowY = 'unset';
 }
 /*  SHARE BUTTON POPUP SCRIPT */
-function sharepopup(popup){
+function sharepopup(popup) {
     document.querySelector(popup).style.display = 'block';
-    document.querySelector('.bg-overlay').style.display ='block';
+    document.querySelector('.bg-overlay').style.display = 'block';
     document.querySelector('.bg-overlay').style.background = 'none';
 }
 // checkbox hovers
@@ -21,39 +22,39 @@ function myFunction(x, b, c, _this) {
         document.querySelector(x).style.backgroundColor = '#00A8E8';
         document.querySelector(b).style.color = '#ffffff';
         document.querySelector(c).style.color = '#ffffff';
-    } else  {
+    } else {
         document.querySelector(x).style.backgroundColor = '#F4F5F4';
         document.querySelector(b).style.color = '#263E33';
         document.querySelector(c).style.color = '#65756E';
     }
-  }
+}
 
 
 // bank exchange rate scrolldown & scroll up menu
 
-function scrollDown(table, arrowdown, arrowup){
+function scrollDown(table, arrowdown, arrowup) {
     document.querySelector(table).style.display = 'block';
     document.querySelector(arrowdown).style.display = 'none';
-    document.querySelector(arrowup).style.display ='block';
+    document.querySelector(arrowup).style.display = 'block';
 }
-function scrollUp(table, arrowdown, arrowup){
+
+function scrollUp(table, arrowdown, arrowup) {
     document.querySelector(table).style.display = 'none';
     document.querySelector(arrowdown).style.display = 'block';
-    document.querySelector(arrowup).style.display ='none';
+    document.querySelector(arrowup).style.display = 'none';
 }
 
 // view more banks and kiosk options 
 function viewMore1() {
     var count;
-    for(count=1;count<6;count++)
-    {
-        document.getElementById('row-'+count+'').style.display = 'block';
+    for (count = 1; count < 6; count++) {
+        document.getElementById('row-' + count + '').style.display = 'block';
     }
 };
+
 function viewMore2() {
     var count;
-    for(count=1;count<6;count++)
-    {
-        document.getElementById('row-'+count+'').style.display = 'block';
+    for (count = 1; count < 6; count++) {
+        document.getElementById('row-' + count + '').style.display = 'block';
     }
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,112 @@
-;(() => {
-  const tabs = document.querySelectorAll('[data-tab-target]')
-  const tabContents = document.querySelectorAll('[data-tab-content]')
+(() => {
+    const tabs = document.querySelectorAll('[data-tab-target]')
+    const tabContents = document.querySelectorAll('[data-tab-content]')
 
-  tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
-      tabContents.forEach((tabContent) => {
-        tabContent.classList.remove('active')
-      })
-      tabs.forEach((tab) => {
-        tab.classList.remove('active')
-      })
-      tab.classList.add('active')
-      target.classList.add('active')
-    })
-  })
-
-  // filter dropdown
-  function filterDropdown() {
-    if (!document.querySelector('.filter-dropdown')) return
-
-    const dropdowns = document.querySelectorAll('.filter-dropdown')
-
-    dropdowns.forEach((dropdown) => {
-      const trigger = dropdown.querySelector('[dropdown-trigger]')
-      const options = dropdown.querySelectorAll('.filter-options button')
-
-      trigger.addEventListener('click', () => {
-        dropdown.classList.toggle('active')
-      })
-
-      options.forEach((option) => {
-        option.addEventListener('click', () => {
-          // update filter button chosen option
-          const optionInnerContent = option.innerHTML
-          trigger.querySelector('div').innerHTML = optionInnerContent
-
-          // close dropdown
-          dropdown.classList.remove('active')
-
-          // submit api call from here
-          console.log(option.value)
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.tabTarget)
+            tabContents.forEach((tabContent) => {
+                tabContent.classList.remove('active')
+            })
+            tabs.forEach((tab) => {
+                tab.classList.remove('active')
+            })
+            tab.classList.add('active')
+            target.classList.add('active')
         })
-      })
     })
-  }
 
-/*  function currencyDropdown() {
-    if (!document.querySelector('.dropdown')) return
+    // filter dropdown
+    function filterDropdown() {
+        if (!document.querySelector('.filter-dropdown')) return
 
-    const dropdowns = document.querySelectorAll('.dropdown')
+        const dropdowns = document.querySelectorAll('.filter-dropdown')
 
-    dropdowns.forEach((dropdown) => {
-      const trigger = dropdown.querySelector('[dropdown-trigger]')
-      const options = dropdown.querySelectorAll('.dropdown-options button')
+        dropdowns.forEach((dropdown) => {
+            const trigger = dropdown.querySelector('[dropdown-trigger]')
+            const options = dropdown.querySelectorAll('.filter-options button')
 
-      trigger.addEventListener('click', () => {
-        dropdown.classList.toggle('active')
-      })
+            trigger.addEventListener('click', () => {
+                dropdown.classList.toggle('active')
+            })
 
-      options.forEach((option) => {
-        option.addEventListener('click', () => {
-          // update filter button chosen option
-          const optionInnerContent = option.innerHTML
-          trigger.querySelector('div').innerHTML = optionInnerContent
+            options.forEach((option) => {
+                option.addEventListener('click', () => {
+                    // update filter button chosen option
+                    const optionInnerContent = option.innerHTML
+                    trigger.querySelector('div').innerHTML = optionInnerContent
 
-          // close dropdown
-          dropdown.classList.remove('active')
+                    // close dropdown
+                    dropdown.classList.remove('active')
 
-          // submit api call from here
-          console.log(option.value)
+                    // submit api call from here
+                    console.log(option.value)
+                })
+            })
         })
-      })
-    })
-  }
-*/
-  // show more of table content
-  function showMore() {
-    if (!document.querySelector('.show-more')) return
+    }
 
-    const btns = document.querySelectorAll('.show-more')
+    /*  function currencyDropdown() {
+        if (!document.querySelector('.dropdown')) return
 
-    btns.forEach((btn) => {
-      const table = btn.closest('.exchange').querySelector('.exchange-table')
-      btn.addEventListener('click', () => {
-        table.classList.toggle('active')
-      })
-    })
-  }
+        const dropdowns = document.querySelectorAll('.dropdown')
 
-  // datepicker
-  function datepicker() {
-    if (!document.querySelector('.filter-by-date')) return
+        dropdowns.forEach((dropdown) => {
+          const trigger = dropdown.querySelector('[dropdown-trigger]')
+          const options = dropdown.querySelectorAll('.dropdown-options button')
 
-    const datefilters = document.querySelectorAll('.filter-by-date')
-    datefilters.forEach((filter) => {
-      const input = filter.querySelector('input')
-      const chosenDate = filter.querySelector('button span')
+          trigger.addEventListener('click', () => {
+            dropdown.classList.toggle('active')
+          })
 
-      input.addEventListener('change', () => {
-        const inputValue = input.value
-        chosenDate.innerHTML = inputValue
-      })
-    })
-  }
+          options.forEach((option) => {
+            option.addEventListener('click', () => {
+              // update filter button chosen option
+              const optionInnerContent = option.innerHTML
+              trigger.querySelector('div').innerHTML = optionInnerContent
 
-  filterDropdown()
-  // currencyDropdown()
-  showMore()
-  datepicker()
+              // close dropdown
+              dropdown.classList.remove('active')
+
+              // submit api call from here
+              console.log(option.value)
+            })
+          })
+        })
+      }
+    */
+    // show more of table content
+    function showMore() {
+        if (!document.querySelector('.show-more')) return
+
+        const btns = document.querySelectorAll('.show-more')
+
+        btns.forEach((btn) => {
+            const table = btn.closest('.exchange').querySelector('.exchange-table')
+            btn.addEventListener('click', () => {
+                table.classList.toggle('active')
+            })
+        })
+    }
+
+    // datepicker
+    function datepicker() {
+        if (!document.querySelector('.filter-by-date')) return
+
+        const datefilters = document.querySelectorAll('.filter-by-date')
+        datefilters.forEach((filter) => {
+            const input = filter.querySelector('input')
+            const chosenDate = filter.querySelector('button span')
+
+            input.addEventListener('change', () => {
+                const inputValue = input.value
+                chosenDate.innerHTML = inputValue
+            })
+        })
+    }
+
+    filterDropdown()
+        // currencyDropdown()
+    showMore()
+    datepicker()
 })()
